3a08b922ad3b2448497d7b5c702c1ec83bde65c9
Sridhar Ratnakumar <srid@srid.ca>
2024-04-04 09:59:27 +1100

    wip

---
+++ flake-module.nix
@@ -0,0 +1,135 @@
+# TODO: Upstream to separate repo (for reuse in other repos)
+{ self, lib, inputs, flake-parts-lib, ... }:
+
+let
+  inherit (flake-parts-lib)
+    mkPerSystemOption;
+in
+{
+  options = {
+    perSystem = mkPerSystemOption
+      ({ config, self', inputs', pkgs, system, ... }: {
+        options = {
+          rust-project.overrideCraneArgs = lib.mkOption {
+            type = lib.types.functionTo lib.types.attrs;
+            default = _: { };
+            description = "Override crane args for the rust-project package";
+          };
+
+          rust-project.rustBuildInputs = lib.mkOption {
+            type = lib.types.listOf lib.types.package;
+            default = [ ] ++ lib.optionals pkgs.stdenv.isLinux (with pkgs; [
+              webkitgtk_4_1
+              xdotool
+            ]) ++ lib.optionals pkgs.stdenv.isDarwin (
+              with pkgs.darwin.apple_sdk.frameworks; [
+                IOKit
+                Carbon
+                WebKit
+                Security
+                Cocoa
+              ]
+            );
+            description = "(Runtime) buildInputs for the cargo package";
+          };
+
+          rust-project.rustToolchain = lib.mkOption {
+            type = lib.types.package;
+            description = "Rust toolchain to use for the rust-project package";
+            default = (pkgs.rust-bin.fromRustupToolchainFile (self + /rust-toolchain.toml)).override {
+              extensions = [
+                "rust-src"
+                "rust-analyzer"
+                "clippy"
+              ];
+            };
+          };
+
+          rust-project.craneLib = lib.mkOption {
+            type = lib.types.lazyAttrsOf lib.types.raw;
+            default = (inputs.crane.mkLib pkgs).overrideToolchain config.rust-project.rustToolchain;
+          };
+
+          rust-project.src = lib.mkOption {
+            type = lib.types.path;
+            description = "Source directory for the rust-project package";
+            # When filtering sources, we want to allow assets other than .rs files
+            # TODO: Don't hardcode these!
+            default = lib.cleanSourceWith {
+              src = self; # The original, unfiltered source
+              filter = path: type:
+                # Default filter from crane (allow .rs files)
+                (config.rust-project.craneLib.filterCargoSources path type)
+              ;
+            };
+          };
+        };
+        config =
+          let
+            cargoToml = builtins.fromTOML (builtins.readFile (self + /Cargo.toml));
+            inherit (cargoToml.package) name version;
+            inherit (config.rust-project) rustToolchain craneLib src;
+
+            # Crane builder for Dioxus projects projects
+            craneBuild = rec {
+              args = {
+                inherit src;
+                pname = name;
+                version = version;
+                buildInputs = [
+                ] ++ config.rust-project.rustBuildInputs;
+                nativeBuildInputs = with pkgs;[
+                  pkg-config
+                  makeWrapper
+                ];
+                # glib-sys fails to build on linux without this
+                # cf. https://github.com/ipetkov/crane/issues/411#issuecomment-1747533532
+                strictDeps = true;
+              };
+              cargoArtifacts = craneLib.buildDepsOnly args;
+              buildArgs = args // {
+                inherit cargoArtifacts;
+              };
+              package = craneLib.buildPackage
+                (buildArgs // config.rust-project.overrideCraneArgs buildArgs);
+
+              check = craneLib.cargoClippy (args // {
+                inherit cargoArtifacts;
+                cargoClippyExtraArgs = "--all-targets --all-features -- --deny warnings";
+              });
+
+              doc = craneLib.cargoDoc (args // {
+                inherit cargoArtifacts;
+              });
+            };
+
+            rustDevShell = pkgs.mkShell {
+              shellHook = ''
+                # For rust-analyzer 'hover' tooltips to work.
+                export RUST_SRC_PATH="${rustToolchain}/lib/rustlib/src/rust/library";
+              '';
+              buildInputs = [
+                pkgs.libiconv
+              ] ++ config.rust-project.rustBuildInputs;
+              packages = [
+                rustToolchain
+              ];
+            };
+          in
+          {
+            # Rust package
+            packages.${name} = craneBuild.package;
+            packages."${name}-doc" = craneBuild.doc;
+
+            checks."${name}-clippy" = craneBuild.check;
+
+            # Rust dev environment
+            devShells.${name} = pkgs.mkShell {
+              inputsFrom = [
+                rustDevShell
+              ];
+            };
+          };
+      });
+  };
+}
---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/default";
+
+    # Dev tools
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+
+    # TODO: Move to flake-module.nix?
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    # https://github.com/ipetkov/crane/issues/527
+    crane.url = "github:ipetkov/crane/2c653e4478476a52c6aa3ac0495e4dea7449ea0e"; # Cargo.toml parsing is broken in newer crane (Mar 24)
+    crane.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
+      imports = [
+        inputs.treefmt-nix.flakeModule
+        ./flake-module.nix
+      ];
+      perSystem = { config, self', pkgs, lib, system, ... }: {
+        _module.args.pkgs = import inputs.nixpkgs {
+          inherit system;
+          overlays = [
+            inputs.rust-overlay.overlays.default
+          ];
+        };
+
+        # Add your auto-formatters here.
+        # cf. https://numtide.github.io/treefmt/
+        treefmt.config = {
+          projectRootFile = "flake.nix";
+          programs = {
+            nixpkgs-fmt.enable = true;
+            rustfmt.enable = true;
+          };
+        };
+
+        devShells.default = self'.devShells.nix_rs;
+      };
+    };
+}
