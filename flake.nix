f299b762541ecfe0aab9491bf8e249d9f7eab8d8
Sridhar Ratnakumar <srid@srid.ca>
2024-04-04 11:15:06 +1100

    upstream

--- flake.nix
+++ flake.nix
@@ -3,15 +3,11 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     systems.url = "github:nix-systems/default";
+    rust-flake.url = "github:juspay/rust-flake/init";
+    rust-flake.inputs.nixpkgs.follows = "nixpkgs";
 
     # Dev tools
     treefmt-nix.url = "github:numtide/treefmt-nix";
-
-    # TODO: Move to flake-module.nix?
-    rust-overlay.url = "github:oxalica/rust-overlay";
-    # https://github.com/ipetkov/crane/issues/527
-    crane.url = "github:ipetkov/crane/2c653e4478476a52c6aa3ac0495e4dea7449ea0e"; # Cargo.toml parsing is broken in newer crane (Mar 24)
-    crane.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = inputs:
@@ -19,13 +15,13 @@
       systems = import inputs.systems;
       imports = [
         inputs.treefmt-nix.flakeModule
-        ./flake-module.nix
+        inputs.rust-flake.flakeModules.default
       ];
       perSystem = { config, self', pkgs, lib, system, ... }: {
         _module.args.pkgs = import inputs.nixpkgs {
           inherit system;
           overlays = [
-            inputs.rust-overlay.overlays.default
+            inputs.rust-flake.inputs.rust-overlay.overlays.default
           ];
         };
 
